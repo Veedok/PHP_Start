1ddc8cd435a1c1f2080edc71d500ea052cefc422
Anatoly <Konakov88@gmail.com>
2021-10-06 14:54:54 +0300

    Work1

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+    $title = 'Здесь должен быть title';
+    $h1 = 'Здесь должен быть h1';
+    $year = date("Y");
+
+   echo "<!DOCTYPE html>
+   <html lang='ru'>
+   <head>
+       <meta charset='UTF-8'>
+       <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+       <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+       <title>$title</title>
+   </head>
+   <body>
+        <h1>$h1</h1>
+        <h2>Сейчас $year год</h2>
+        <a href='Task1.jpg' target='_blank'>Задание 1</a>
+        <a href='task2.php' target='_blank'>Задание 2</a>
+        <a href='task3.php' target='_blank'>Задание 3</a>
+        <a href='task5.php' target='_blank'>Задание 5</a>
+   </body>
+   </html>";
