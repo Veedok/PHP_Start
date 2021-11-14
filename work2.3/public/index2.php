42869c3478cc59cdd1b39527ed28c7bec682ee33
Anatoly <Konakov88@gmail.com>
2021-11-14 22:16:23 +0300

    Комит

--- work2.3/public/index2.php
+++ work2.3/public/index2.php
@@ -1,2 +1,12 @@
 <?php
-echo('Helow world');
+$link = mysqli_connect('127.0.0.1:3306', 'root', 'root', 'php_start');
+$result = mysqli_query($link, "SELECT * FROM images ORDER BY countClick DESC");
+var_dump($result);
+while ($row = mysqli_fetch_assoc($result)){
+    echo ("<a href='/singleimg.php?img_id=" . $row["id"] . "'>
+                <div class='item'>
+                    <img class='product_img' src=" . $row["imgPath"] . " alt='img'>
+                    <p>" . $row["imgName"]  . "</p>
+                </div>
+            </a>");        
+    }
